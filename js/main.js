9339b537cd1ca9c7cc705c54b96a9283222f2b7e
Andrei Ladonin <dron201202@gmail.com>
2024-05-20 20:05:14 +0400

    end mobile develop

--- js/main.js
+++ js/main.js
@@ -20,3 +20,10 @@ tabItem.forEach((el)=> {
     
     })
 })
+
+const menuBtn = document.querySelector('.menu__btn')
+const menu = document.querySelector('.menu')
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu--active')
+})
